c445a06b208d8d983cefc63cd052dc88b304826b
selakap <38026978+selakap@users.noreply.github.com>
2022-05-18 13:23:54 +0530

    remove unwanted lines

--- thread-analyze.sh
+++ thread-analyze.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 if [ "$#" -ne 3 ]; then
-        echo "usage: usage: sh thread-analyze.sh <PID> <timegap> <delete_files_older_than_given_minutes>"
+        echo "usage: sh thread-analyze.sh <PID> <timegap> <delete_files_older_than_given_minutes>"
         exit
 fi
 declare -i i=1
 while :
 do
-
-
           jstack -l $1 > thread_dump_`date "+%F-%T"`.txt &
           ps --pid $1 -Lo pid,tid,%cpu,time,nlwp,c > thread_usage_`date "+%F-%T"`.txt &
 
@@ -15,5 +13,4 @@ do
           sleep $2
   find ./ -maxdepth 1 -type f -name "thread_*.txt" -amin +$3 -delete
   i+=1
-  # find ./ -type f -regex '.*-\d{8}\.txt' -amin +$4 -delete
 done
